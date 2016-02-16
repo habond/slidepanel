d7b28f050866d43b1cb62e4615e4fd096c0c4de1
Henry Bond <habond@gmail.com>
2016-02-15 22:07:08 -0500

    added animations

--- app/slidepanels.js
+++ app/slidepanels.js
@@ -3,13 +3,15 @@
 
 	angular.module('ui-slidepanel', [])
 
-	.factory('slidepanelFactory', function($document, $q, $compile, $controller, $rootScope, $templateRequest) {
+	.factory('slidepanelFactory', function($document, $q, $compile, $controller, $timeout, $rootScope, $templateRequest) {
 
 		return {
 			open: SlidePanelOpenFn
 		};
 
 		function SlidePanelOpenFn(def) {
+
+			var body = $document.find('body').eq(0);
 
 			var events = {
 				opened: $q.defer(),
@@ -48,8 +50,12 @@
 			}
 
 			function DestoryInstance() {
-				instance.scope.$destroy();
-				instance.elem.remove();
+				instance.scope.$broadcast('spClosing');
+				$timeout(function(){
+					instance.scope.$destroy();
+					instance.elem.remove();
+					body.removeClass('sp-overflow-hidden');
+				},500);
 			}
 
 			function CreateInstance(template) {
@@ -63,16 +69,16 @@
 				var elem = instance.elem = angular
 					.element('<div slidepanel></div>')
 					.attr({
-						'dismiss': '_spInstance.dismiss',
-						'size': def.size
+						'position': def.position,
+						'size': def.size,
+						'dismiss': '_spInstance.dismiss'
 					})
 					.html(template);
 
 				$compile(elem)(scope);
-				var body = $document.find('body').eq(0);
+				body.addClass('sp-overflow-hidden');
 				body.append(elem);
-
-				events.opened.resolve();
+				events.opened.resolve(instance);
 			}
 
 			function TemplateRequestFail(err) {
@@ -88,29 +94,66 @@
 
 	})
 
-	.directive('slidepanel', function SlidePanelDirective() {
+	.directive('slidepanel', function SlidePanelDirective($document,$timeout) {
 		return {
-			restrict: 'A',
+			restrict: 'AE',
 			scope: {
+				position: '@',
 				size: '@',
-				dismiss: '&dismiss'
+				dismiss: '&'
 			},
 			transclude: true,
 			link: function(scope, element, attrs, controller, transcludeFn) {
+
+				var dismissFn = scope.dismiss();
+
 				var panel = angular
 					.element('<div></div>')
-					.addClass('sp-panel sp-left')
+					.addClass('sp-panel')
+					.addClass('sp-' + attrs.position)
+					.attr('style', getStyle(false))
 					.append(transcludeFn());
-				panel[0].style.width = attrs.size;
+
+				$timeout(function(){
+					panel.attr('style', getStyle(true));
+				}, 100);
+
+				function getStyle(open) {
+					switch(attrs.position) {
+						case "left": case "right":
+							return 'width:' + attrs.size + ';' + attrs.position + ':' + (open ? 0 : '-' + attrs.size);
+						case "top": case "bottom":
+							return 'height:' + attrs.size + ';' + attrs.position + ':' + (open ? 0 : '-' + attrs.size);
+					}
+				}
 
 				var dim = angular
 					.element('<div></div>')
 					.addClass('sp-dim sp-open')
-					.on('click', scope.dismiss());
+					.on('click', dismissFn);
+
+				var keyDownListener = function(e) {
+					switch(e.which){
+						case 27:
+							dismissFn(e);
+							break;
+					}
+				};
+
+				$document.on('keydown', keyDownListener);
+
+				scope.$on('spClosing', function(){
+					panel.attr('style', getStyle(false));
+				});
+
+				scope.$on('$destroy', function() {
+					$document.off('keydown', keyDownListener);
+				});
 
 				element
 					.append(panel)
 					.append(dim);
+
 			}
 		};
 	});
